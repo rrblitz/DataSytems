6b4bcd29b131d9969dfde07b1b4915bdd7549b70
rrblitz <rrblitz@indiana.edu>
2018-10-03 09:22:30 -0400

    10/3 update

--- App/common.php
+++ App/common.php
@@ -3,6 +3,10 @@
 chdir(__DIR__);
 set_include_path (__DIR__);
 require 'environment.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST'
+&& stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
+  $_POST = json_decode(file_get_contents('php://input'), true);
+}
 /** MODELS **/
 require 'models/Work.php';
 require 'models/Team.php';
--- Public/api/work.php
+++ Public/api/work.php
@@ -1,13 +1,19 @@
 <?php
 
+
 require '../../app/common.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  require 'workPost.php';
+  exit;
+}
 
 $taskId = intval($_GET['taskId'] ?? 0);
 
 if ($taskId < 1) {
   throw new Exception('Invalid Task ID');
 }
-
 
 // 1. Go to the database and get all work associated with the $taskId
 $workArr = Work::getWorkByTaskId($taskId);
