78de49dfde0b626650025d87b0644b71e41d4d4a
rrblitz <rrblitz@indiana.edu>
2018-10-10 14:45:57 -0400

    attempt

--- App/models/Work.php
+++ App/models/Work.php
@@ -8,13 +8,13 @@ class Work
   public $stop;   //'YYYY-MM-DD', needs to be calculated
   public $hours;
   public $completion_estimate;
-  
+
   public function __construct($row) {
     $this->id = isset($row['id']) ? intval($row['id']) : null;
     $this->task_id = intval($row['task_id']);
     $this->team_id = intval($row['team_id']);
     $this->start = $row['start_date'];
-    $this->hours = floatval($row['hours']);
+  
     // Calculate stop date
     $hours = floor($this->hours);
     $mins = intval(($this->hours - $hours) * 60); // Take advantage of one decimal place
